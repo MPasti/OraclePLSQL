bbedb34e252f8d04f073e242c25cdb4a3af1b1e2
Matheus Pasti <117304327+MPasti@users.noreply.github.com>
2024-09-17 18:52:18 -0300

    Update functions.sql

--- functions.sql
+++ functions.sql
@@ -1,3 +1,55 @@
+//INNER JOIN
+SELECT columns
+FROM table1
+INNER JOIN table2
+ON table1.column = table2.column;
+
+SELECT e.employee_id, e.first_name, d.department_name
+FROM employees e
+INNER JOIN departments d
+ON e.department_id = d.department_id;
+
+//natural join
+
+//automaticamente cria uma junção entre as tabelas com base em todas as colunas com o mesmo nome e tipo nas duas tabelas. Ou seja, você não precisa especificar uma condição, pois o Oracle utiliza as colunas comuns para realizar a junção.
+SELECT columns
+FROM table1
+NATURAL JOIN table2;
+
+//JOIN ... USING
+
+SELECT columns
+FROM table1
+JOIN table2 USING (column1);
+
+SELECT e.employee_id, e.first_name, d.department_name
+FROM employees e
+JOIN departments d
+USING (department_id);
+
+//JOIN ON
+SELECT columns
+FROM table1
+JOIN table2
+ON table1.column = table2.column;
+
+//OUTER JOIN
+SELECT columns
+FROM table1
+LEFT OUTER JOIN table2
+ON table1.column = table2.column;
+
+
+SELECT e.first_name, d.department_name
+FROM employees AS e
+JOIN departments AS d
+ON e.department_id = d.department_id;
+
+SELECT e.first_name, d.department_name
+FROM employees e
+JOIN departments d
+ON e.department_id = d.department_id;
+
 SELECT UPPER(nome), LOWER(sobrenome)
 FROM tb_funcionarios;
 
@@ -53,3 +105,44 @@ FROM tb_produtos;
 
 SELECT nm_produto, LENGTH(nm_produto) AS 'Length do produto'
 FROM tb_produtos;
+
+SELECT 'O identificador da ' || descricao_funcao || ' é o ID: ' || id_funcao AS "Descrição da Função"
+FROM TB_FUNCAO;
+
+CREATE OR REPLACE PROCEDURE show_salario(p_emp_id IN NUMBER) IS
+    v_salario employees.salary%TYPE;
+BEGIN
+    SELECT salary INTO v_salario
+    FROM employees
+    WHERE employee_id = p_emp_id;
+    
+    DBMS_OUTPUT.PUT_LINE('O salário do empregado ' || p_emp_id || ' é: ' || v_salario);
+END;
+
+SELECT 'Hello, World!' AS greeting
+FROM DUAL;
+
+
+SELECT 2 * 3 AS resultado1, SYSDATE AS data_atual, USER AS nome_usuario
+FROM DUAL;
+
+SELECT * 
+FROM employees 
+WHERE employee_id = &emp_id;
+
+
+//Se você quiser que a variável seja definida apenas uma vez e reutilizada, você pode usar &&:
+SELECT * 
+FROM employees 
+WHERE employee_id = &&emp_id;
+
+
+
+// O LIKE permite o uso de caracteres curinga (% e _) para criar esses padrões flexíveis.
+SELECT nome_departamento
+FROM TB_DEPARTAMENTO
+WHERE nome_departamento LIKE 'Eng%';
+
+SELECT nome_departamento
+FROM TB_DEPARTAMENTO
+WHERE nome_departamento LIKE '%ing%';
